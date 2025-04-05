fbb37b86ff86895d7561ee66a946057a64cdcb0d
Kaitlyn-Clevidence <kclevidence@unr.edu>
2025-04-04 20:39:43 -0700

    doing lots of random testing

--- flaskApp.py
+++ flaskApp.py
@@ -1,14 +1,22 @@
-from flask import Flask, redirect, url_for, render_template 
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "<h1>wassup</h1>"
+def landing():
+    return render_template("landingPage.html")
 
-@app.route("/login/", methods = ["POST", 'GET'])
+@app.route("/login/", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # handle login logic here
+        return redirect(url_for(""))
+    return render_template("login.html")
+
+@app.route("/<username>")
+def personalView(username):
+    #heres where we start referencing the database for their personal events and settings \
     pass
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
